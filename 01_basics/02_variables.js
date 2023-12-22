cac37326a21642ed40a53f0d90bd3c0de32212ce
Souri20 <139069913+Souri20@users.noreply.github.com>
2023-12-22 20:19:36 +0000

    changes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,9 @@
+const name = 'Sourabh'
+let surname = 'Kumbhar'
+var surname1 = 'raje'
+
+
+surname = "Raje"
+
+
+console.table([name,surname]);
---
+++ 01_basics/03_Dataconversion.js
@@ -0,0 +1,15 @@
+let str = 'null'
+
+let toNum = Number(str)
+console.log(toNum);
+
+let boo = 1
+let tobool = Boolean(boo)
+
+console.log(tobool)
+
+let num =  20
+
+let toStr = String(num)
+
+console.log(typeof(toStr))
